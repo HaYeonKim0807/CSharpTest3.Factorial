341ed641159c9edb854ed120b5bf885cc5e5ec89
HaYeonKim0807 <kh701201@naver.com>
2021-03-06 05:16:19 +0900

    Hello

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace sdf
+{
+    class Program
+    {
+        static int Factorial(int n)
+        {
+            int ret = 1;
+
+            for (int m = 1; m <= n; m++)
+
+            {
+                ret *= m;
+            }
+
+            return ret;
+
+        }
+
+        static void Main(string[] args)
+        {
+            int n = 5;
+            int ret = Factorial(n);
+            Console.WriteLine(ret);
+
+        }
+    }
+}
